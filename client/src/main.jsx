470401dbd886f7cbf1fab6b6bc183e63ba5f9ced
Shivansh Rai <shivanshrai282@gmail.com>
2026-09-12 20:28:52 +0530

    feat: connect frontend to live Render backend with proxy and axios baseURL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
 import App from "./App.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./index.css";
+
+if (import.meta.env.VITE_API_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
